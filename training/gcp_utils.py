2eb4efbea0f2cff97ba175009ba341ec15debb5d
Paul-Henri <paul-henri@Paul-Henris-MacBook-Pro.local>
2024-09-12 16:36:06 +0200

    reseted gcp fetch

--- training/gcp_utils.py
+++ training/gcp_utils.py
@@ -60,6 +60,7 @@ def fetch_checkpoint_from_gcs(bucket_name, checkpoint_path, output_dir, key_file
     blobs = bucket.list_blobs(prefix=checkpoint_path)
 
     for blob in blobs:
+        print(f"Downloading: {blob.name}")
         if blob.name.endswith("/"):
             continue
         
