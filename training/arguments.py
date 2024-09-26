b37161c52737ef4b3200bece46787dd4cb548357
Paul-HenriBJT <phbiojout@gmail.com>
2024-09-26 08:12:26 +0200

    added load_precomputed_dataset_from_hub

--- training/arguments.py
+++ training/arguments.py
@@ -343,6 +343,10 @@ class DataTrainingArguments:
         default=None,
         metadata={"help": "The name of the dataset to load from Hugging Face Hub."},
     )
+    load_precomputed_dataset_from_hub: bool = field(
+        default=False,
+        metadata={"help": "Whether to load the precomputed dataset from the Hub."},
+    )
     
 
 @dataclass
